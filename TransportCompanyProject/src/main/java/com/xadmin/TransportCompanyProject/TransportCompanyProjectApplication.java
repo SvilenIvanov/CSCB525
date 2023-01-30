b331b782abf1f13967aa95b4d6735c5a5d3349c8
Svilen <svilen.ivanov01@gmail.com>
2023-01-30 23:34:48 +0200

    Updated the main class, added a new company to the companyService

--- TransportCompanyProject/src/main/java/com/xadmin/TransportCompanyProject/TransportCompanyProjectApplication.java
+++ TransportCompanyProject/src/main/java/com/xadmin/TransportCompanyProject/TransportCompanyProjectApplication.java
@@ -1,14 +1,26 @@
 package com.xadmin.TransportCompanyProject;
 
+import com.xadmin.TransportCompanyProject.services.CompanyService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class TransportCompanyProjectApplication {
 
+	@Autowired
+	private CompanyService companyService;
+
+
 	public static void main(String[] args) {
-		SpringApplication.run(TransportCompanyProjectApplication.class, args);
+		ConfigurableApplicationContext context = SpringApplication.run(TransportCompanyProjectApplication.class, args);
+		TransportCompanyProjectApplication app = context.getBean(TransportCompanyProjectApplication.class);
+		app.run();
 
 	}
+	private void run() {
+		companyService.createCompany("XYZCompany2");
 
+	}
 }
